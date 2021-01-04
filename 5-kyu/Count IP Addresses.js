386d21422bdf57ced75b27da0511ddc6a115d46d
dev stefanCho <61320923+devstefancho@users.noreply.github.com>
2021-01-04 17:50:30 +0900

    Create Count IP Addresses.js

---
+++ 5-kyu/Count IP Addresses.js
@@ -0,0 +1,16 @@
+/****
+Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
+
+All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
+
+Examples
+ipsBetween("10.0.0.0", "10.0.0.50")  ===   50 
+ipsBetween("10.0.0.0", "10.0.1.0")   ===  256 
+ipsBetween("20.0.0.10", "20.0.1.0")  ===  246
+****/
+function ipsBetween(start, end){
+  const amount = [Math.pow(256, 3), Math.pow(256, 2), Math.pow(256, 1), 1]
+  sArr = start.split('.');
+  eArr = end.split('.');
+  return eArr.reduce((acc, el, idx) => (el === sArr[idx] ? acc : acc + (el - sArr[idx]) * amount[idx]) , 0)
+}
